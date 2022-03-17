c185c15aae09f2056497026d1dc817bad6a54ea7
ajit <palajitsingh91@gmail.com>
2022-03-17 18:27:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main()
+	{
+		int[] nums = new int[5] { 10, 15, 16, 8, 6 };
+
+		Console.WriteLine(nums.Max());
+		Console.WriteLine(nums.Min());
+		Console.WriteLine(nums.Sum());
+		Console.WriteLine(nums.Average());
+	}
+}
